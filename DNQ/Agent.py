84026427bc4adbf56fb36abbadd1d36589608cb1
robbyyt <49028058+robbyyt@users.noreply.github.com>
2020-12-03 16:32:18 +0200

    models

--- DNQ/Agent.py
+++ DNQ/Agent.py
@@ -1,110 +1,119 @@
 
 
-#ADDJUSTABLE VARIABLES
-MAX_MEM_SIZE =10_000# MEMORIA DEQUE-ULUI
-MINIBATCH_SIZE =64# NR DE BATCH-URI CARE LE IA LA FIT 
-MIN_REPLAY_MEMORY_SIZE =  100  # CAND INCEPE SA IA LA FIT
-DISCOUNT = 0.90# PT Q LEARNING 
-UPDATE_TARGET_EVERY = 5 # CAND UPDATEAZA AL DOILEA MODEL 
-MODEL_NAME = "SPACE"
-LR =0.00001
-TAU =0.01
-print('Batch size is ',MINIBATCH_SIZE)
-import tensorflow as tf
-
+# ADDJUSTABLE VARIABLES
+from tensorflow.keras.layers import Activation, Dense, MaxPooling2D, Conv2D, Dropout, Flatten
+import time
+import random
+import numpy as np
+from tensorflow.keras.optimizers import Adam
+from collections import deque
+from tensorflow.keras.models import Sequential
 from tensorflow.keras.callbacks import TensorBoard
+import tensorflow as tf
+MAX_MEM_SIZE = 10_000  # MEMORIA DEQUE-ULUI
+MINIBATCH_SIZE = 64  # NR DE BATCH-URI CARE LE IA LA FIT
+MIN_REPLAY_MEMORY_SIZE = 1000  # CAND INCEPE SA IA LA FIT
+DISCOUNT = 0.90  # PT Q LEARNING
+UPDATE_TARGET_EVERY = 5  # CAND UPDATEAZA AL DOILEA MODEL
+MODEL_NAME = "SPACE"
+LR = 0.00001
+TAU = 0.01
+print('Batch size is ', MINIBATCH_SIZE)
+
 #from tensorB import ModifiedTensorBoard
 gpus = tf.config.experimental.list_physical_devices('GPU')
 if gpus:
-  try:
-    for gpu in gpus:
-      tf.config.experimental.set_memory_growth(gpu, True)
-  except RuntimeError as e:
-    print(e)
+    try:
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+    except RuntimeError as e:
+        print(e)
 
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Activation,Dense,MaxPooling2D,Conv2D, Dropout,Flatten
-from collections import deque
-from tensorflow.keras.optimizers import Adam
-import numpy as np
-import random
-import time 
 
 class Agent:
-    def __init__(self,nr_actions,shape): 
+    def __init__(self, nr_actions, shape):
         self.counter = 1
-        self.model = self.create_model_2(nr_actions,shape)
-        self.replay_memory = deque(maxlen =MAX_MEM_SIZE)
-        self.target_update_counter = 0 
-        self.target_model = self.create_model_2(nr_actions,shape)
+        self.model = self.create_model_2(nr_actions, shape)
+        self.replay_memory = deque(maxlen=MAX_MEM_SIZE)
+        self.target_update_counter = 0
+        self.target_model = self.create_model_2(nr_actions, shape)
         log_dir = f"logs/{MODEL_NAME}-{int(self.counter)}"
         self.tensorboard = TensorBoard(log_dir=log_dir)
 
-    def create_model(self,nr_actions,shape):
+    def create_model(self, nr_actions, shape):
         model = Sequential()
-        model.add( Conv2D(32,kernel_size=8,strides=8,input_shape = shape) )
+        model.add(Conv2D(32, kernel_size=8, strides=8, input_shape=shape))
         model.add(Activation("relu"))
-        
-        model.add(MaxPooling2D(2,2))
+
+        model.add(MaxPooling2D(2, 2))
         model.add(Dropout(0.2))
-        
-        model.add( Conv2D(32,kernel_size=4,strides=4)   )
+
+        model.add(Conv2D(32, kernel_size=4, strides=4))
         model.add(Activation("relu"))
-        model.add(MaxPooling2D(2,2))
+        model.add(MaxPooling2D(2, 2))
         model.add(Dropout(0.2))
         model.add(Flatten())
         model.add(Dense(64))
-        model.add(Dense(activation="elu",units = nr_actions))
+        model.add(Dense(activation="elu", units=nr_actions))
         adam = Adam(lr=LR)
-        model.compile(loss='categorical_crossentropy',optimizer=adam)
+        model.compile(loss='categorical_crossentropy', optimizer=adam)
         return model
-    def create_model_1(self,nr_actions,shape):
+
+    def create_model_1(self, nr_actions, shape):
         model = Sequential()
-        model.add( Conv2D(32,kernel_size=[8,8],strides=[4,4],input_shape = shape,padding='valid'))
+        model.add(Conv2D(32, kernel_size=[8, 8], strides=[
+                  4, 4], input_shape=shape, padding='valid'))
         model.add(Activation("relu"))
-        model.add( Conv2D(64,kernel_size=[4,4],strides=[2,2],padding='valid'))
+        model.add(Conv2D(64, kernel_size=[4, 4],
+                         strides=[2, 2], padding='valid'))
         model.add(Activation("relu"))
-        model.add( Conv2D(64,kernel_size=[3,3],strides=[2,2],padding='valid'))
+        model.add(Conv2D(64, kernel_size=[3, 3],
+                         strides=[2, 2], padding='valid'))
         model.add(Activation("relu"))
         model.add(Flatten())
         model.add(Dense(units=512))
         model.add(Activation('relu'))
-        model.add(Dense(units = nr_actions))
+        model.add(Dense(units=nr_actions))
         model.add(Activation("relu"))
         adam = Adam(lr=LR)
-        model.compile(loss="mce",optimizer =adam,metrics =['accuracy'])
+        model.compile(loss="mce", optimizer=adam, metrics=['accuracy'])
         return model
-    def create_model_2(self,nr_actions,shape):
+
+    def create_model_2(self, nr_actions, shape):
         model = Sequential()
-        model.add( Conv2D(32,kernel_size=[8,8],strides=[4,4],input_shape = shape,padding='valid'))
+        model.add(Conv2D(32, kernel_size=[8, 8], strides=[
+                  4, 4], input_shape=shape, padding='valid'))
         model.add(Activation("relu"))
-        model.add( Conv2D(64,kernel_size=[4,4],strides=[2,2],padding='valid'))
+        model.add(Conv2D(64, kernel_size=[4, 4],
+                         strides=[2, 2], padding='valid'))
         model.add(Activation("relu"))
-        model.add( Conv2D(64,kernel_size=[3,3],strides=[2,2],padding='valid'))
+        model.add(Conv2D(64, kernel_size=[3, 3],
+                         strides=[2, 2], padding='valid'))
         model.add(Activation("relu"))
         model.add(Flatten())
         model.add(Dense(units=512))
         model.add(Activation('relu'))
-        model.add(Dense(units = nr_actions))
+        model.add(Dense(units=nr_actions))
         model.add(Activation("relu"))
         adam = Adam(lr=LR)
-        model.compile(loss="mse",optimizer =adam,metrics =['accuracy'])
+        model.compile(loss="mse", optimizer=adam, metrics=['accuracy'])
         return model
+
     def update_replay_memory(self, frame):
         self.replay_memory.append(frame)
 
-    def train(self,terminal_step,step):
+    def train(self, terminal_step, step):
         if len(self.replay_memory) < MIN_REPLAY_MEMORY_SIZE:
-            return 
-        
-        minibatch = np.array(random.sample(self.replay_memory,MINIBATCH_SIZE))
-        cur_states= np.array(minibatch[:,0:1].tolist())[:,0]
+            return
+
+        minibatch = np.array(random.sample(self.replay_memory, MINIBATCH_SIZE))
+        cur_states = np.array(minibatch[:, 0:1].tolist())[:, 0]
         current_qs_list = self.model.predict(cur_states)
-        new_current_states = np.array(minibatch[:,3:4].tolist())[:,0]
+        new_current_states = np.array(minibatch[:, 3:4].tolist())[:, 0]
         future_qs_list = self.target_model.predict(new_current_states)
-        X=[]
-        Y=[]
-        
+        X = []
+        Y = []
+
         for index, (current_state, action, reward, new_current_state, done) in enumerate(minibatch):
 
             if not done:
@@ -112,31 +121,30 @@ class Agent:
                 new_q = reward + DISCOUNT * max_future_q
             else:
                 new_q = reward
-            
 
             current_qs = current_qs_list[index]
             current_qs[action] = new_q
-             
+
             X.append(current_state)
             Y.append(current_qs)
-        
-        self.model.fit(np.array(X), np.array(Y), batch_size=MINIBATCH_SIZE, verbose=0, shuffle=False,callbacks =[self.tensorboard] if terminal_step else None )
+
+        self.model.fit(np.array(X), np.array(Y), batch_size=MINIBATCH_SIZE, verbose=0,
+                       shuffle=False, callbacks=[self.tensorboard] if terminal_step else None)
 
         if terminal_step:
-            self.target_update_counter +=1
-            self.counter +=1
+            self.target_update_counter += 1
+            self.counter += 1
 
         if self.target_update_counter > UPDATE_TARGET_EVERY:
             model_weights = self.model.get_weights()
             target_model_weights = self.target_model.get_weights()
             for i in range(len(model_weights)):
-                target_model_weights[i] = TAU * model_weights[i] + (1 - TAU) * target_model_weights[i]
+                target_model_weights[i] = TAU * model_weights[i] + \
+                    (1 - TAU) * target_model_weights[i]
             self.target_model.set_weights(target_model_weights)
             self.target_update_counter = 0
 
-    def get_qs(self,state):
-        d  = self.model.predict(np.array(state).reshape((1,*state.shape)))
-        #print(d)
+    def get_qs(self, state):
+        d = self.model.predict(np.array(state).reshape((1, *state.shape)))
+        # print(d)
         return d[0]
-
-
--- DNQ/out_train.py
+++ DNQ/out_train.py
@@ -1,74 +1,76 @@
 from tensorflow import keras
 import retro
-import util 
+import util
 import numpy as np
 import matplotlib.pyplot as plt
 import random
-#model = keras.models.load_model('models/SPACE-6')
+model = keras.models.load_model('models/SPACE-142')
 
 possible_actions = np.array(
-    [[0,0,0, 0,0,0 ,1,0,0],#back_movement
-    [0,0,0, 0,0,0 ,0,1,0]#forward_movement
-    ,#[0,0,0, 0,0,0 ,0,0,1],#jump
-    [1,0,0, 0,0,0 ,0,0,0]#attack
-    ]
+    [[0, 0, 0, 0, 0, 0, 1, 0, 0],  # back_movement
+     [0, 0, 0, 0, 0, 0, 0, 1, 0]  # forward_movement
+     ,  # [0,0,0, 0,0,0 ,0,0,1],#jump
+     [1, 0, 0, 0, 0, 0, 0, 0, 0]  # attack
+     ]
 )
 print(possible_actions)
 nr_actions = len(possible_actions)
 
+
 def parse_action(nr):
     return possible_actions[nr]
 
+
 def move_to_first_level(env):
-    act = 1 # move forward
-    for i in range(0,850):
+    act = 1  # move forward
+    for i in range(0, 850):
         env.step(parse_action(act))
         env.render()
 
 
 env = retro.make(game='SpaceInvaders-Atari2600')
 env.reset()
-#move_to_first_level(env)
+# move_to_first_level(env)
 stacked_frames = util.stacked_frames
 stack_frames = util.stack_frames
 
-frame, rew,done,_  = env.step(parse_action(0))
-init =False
-def get_qs(state,model):
-        d  = model.predict(np.array(state).reshape((-1,*state.shape)))
-        #print(d)
-        return d[0]
+frame, rew, done, _ = env.step(parse_action(0))
+init = False
+
+
+def get_qs(state, model):
+    d = model.predict(np.array(state).reshape((-1, *state.shape)))
+    # print(d)
+    return d[0]
+
+
 done = False
 obs = env.reset()
 state, stacked_frames = stack_frames(stacked_frames, obs, True)
 
-state , rew,done,stats =  env.step(parse_action(0))
+state, rew, done, stats = env.step(parse_action(0))
 
 state, stacked_frames = stack_frames(stacked_frames, obs, False)
 
-state , rew,done,stats =  env.step(parse_action(0))
+state, rew, done, stats = env.step(parse_action(0))
 
 state, stacked_frames = stack_frames(stacked_frames, obs, False)
-state , rew,done,stats =  env.step(parse_action(0))
+state, rew, done, stats = env.step(parse_action(0))
 
 state, stacked_frames = stack_frames(stacked_frames, obs, False)
 
 
-
-#qs = get_qs(state,model)
+qs = get_qs(state, model)
 
 
 while not done:
-    
 
-    #qs = get_qs(state,model)
+    qs = get_qs(state, model)
     #print(np.argmax(qs), qs)
-    #action = np.argmax(qs)
-    action = random.randint(0,len(possible_actions)-1)
+    action = np.argmax(qs)
+    #action = random.randint(0, len(possible_actions)-1)
     #d = model.predict(frame)
-    state , rew,done,stats =  env.step(parse_action(action))
-    print(stats)
+    state, rew, done, stats = env.step(parse_action(action))
+    # print(stats)
     state, stacked_frames = stack_frames(stacked_frames, state, False)
     env.render()
-
-
